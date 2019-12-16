ff3d60c6bbf8acc0dc4b97f2d874311ab8fcf8da
anoam <anohinam@gmail.com>
2019-12-16 18:22:41 +0300

    Domain model implementation

--- app/models/domain/merchant.rb
+++ app/models/domain/merchant.rb
@@ -4,4 +4,37 @@
 class Domain::Merchant < ApplicationRecord
   enum status: {active: 0, inactive: 1}
 
+  has_many :transactions, class_name: 'Domain::Merchant::Transaction', dependent: :delete_all
+
+  # Creates a transaction according to given information
+  # @param payment_info [#amount, #processed?, #base_transaction_uuid] base payment data
+  # @return [Result]
+  def accept_payment(payment_info)
+    base_transaction = nil
+
+    if payment_info.base_transaction_uuid
+      base_transaction = transactions.find { |transaction| transaction.uuid == payment_info.base_transaction_uuid }
+      return Result.failure(%i[bad_base_transaction_uuid]) unless base_transaction
+    end
+
+    result = Domain::Merchant::Transaction.build(payment_info, base_transaction)
+
+    if result.success?
+      transactions << result.payload
+      update_transaction_sum
+    end
+
+    result
+  end
+
+  # Removes all payments older than 1 hour
+  def remove_outdated_payments
+    self.transactions = transactions.find_all { |transaction| transaction.created_at > 1.hour.ago }
+  end
+
+  private
+
+  def update_transaction_sum
+    self.transaction_sum = transactions.filter(&:processed?).sum(&:amount)
+  end
 end
--- app/models/domain/merchant/transaction.rb
+++ app/models/domain/merchant/transaction.rb
@@ -5,4 +5,23 @@
 class Domain::Merchant::Transaction < ApplicationRecord
   enum status: {processed: 0, error: 1}
 
+  belongs_to :base, class_name: 'Domain::Merchant::Transaction', foreign_key: :base_transaction_id # rubocop:disable Rails/InverseOf
+
+  # Factory-method
+  # @param info [#amount, #processed?] transaction parameters
+  # @param base [Domain::Merchant::Transaction] base transaction
+  # @return [Result]
+  def self.build(info, base)
+    return Result.failure(%i[bad_amount]) unless info.amount.is_a?(BigDecimal)
+    return Result.failure(%i[bad_amount]) if info.amount.negative?
+
+    Result.success(
+      new(
+        amount: info.amount,
+        status: info.processed? ? :processed : :error,
+        uuid: SecureRandom.uuid,
+        base: base
+      )
+    )
+  end
 end
